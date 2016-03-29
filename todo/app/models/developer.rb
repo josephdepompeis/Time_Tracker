8faba3cd00506c99579d227588253579455d3622
Joseph DePompeis <josephdepompeis@gmail.com>
2016-03-29 16:42:51 -0400

    phase 5 wrapped up, styles to be added

--- todo/app/models/developer.rb
+++ todo/app/models/developer.rb
@@ -3,4 +3,28 @@ class Developer < ActiveRecord::Base
   has_secure_password
   has_many :entries
 
+
+
+  # def project_total_hours
+  #   total_hours = []
+  #   project = self
+  #   project.entries.each do |entry|
+  #     total_hours << entry.duration
+  #   end
+  #   total_hours.sum
+  # end
+  #
+
+  def seven_day
+      weekly_total_hours = []
+      developer = self
+      now = Date.today
+      developer.entries.each do |entry|
+      difference_in_days = (now - entry.date).to_i
+      weekly_total_hours << entry.duration if difference_in_days <= 7
+      end
+      weekly_total_hours.sum
+    end
+
+
 end
